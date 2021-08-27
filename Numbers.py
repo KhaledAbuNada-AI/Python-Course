460dd32851a58626829ee0a97901c88ddea0c7e9
KhaledAudNada <khaled.nada223@gmail.com>
2021-08-27 07:59:32 +0300

    The code for the Numbers lecture has been uploaded
    Khaled Abu Nada

---
+++ Numbers.py
@@ -0,0 +1,31 @@
+from math import *
+number1 = 5
+number2 = -3
+my_list = [1, 10, 15, 100]
+
+# Operations
+# print((number1 + number2) * 2)
+# print(number1 * number2)
+# print(number1 - number2)
+# print(number1 / number2)
+# print(number1 % number2)
+
+# Numbers math
+print(abs(number2))
+print(pow(number1, 2))
+# Numbers Max, Min
+print(max(my_list))
+print(min(my_list))
+
+# Numbers
+print(round(6.9))
+
+# Math Module
+print(floor(6.9))
+print(ceil(3.1))
+print(sqrt(10))
+
+# Converting and merging texts with numbers
+aeg = 20
+text = 'My Aeg ' + str(aeg)
+print(text)
