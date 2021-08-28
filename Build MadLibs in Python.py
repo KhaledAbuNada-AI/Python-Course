8e126c8f9d0cdee566d2d7b41bc46246cce3f59f
KhaledAudNada <khaled.nada223@gmail.com>
2021-08-28 22:02:11 +0300

    The first part of the construction of the MadLibs game project has been completed Khaled Abu Nada

---
+++ Build MadLibs in Python.py
@@ -0,0 +1,8 @@
+programming_language = input("")
+libraries = input("")
+dream = input("")
+
+
+print("I like {}")
+print("And work many libraries such as {}")
+print("I dream of becoming the best engineer in the world {}")
