e7dffe137d727f9f7addf638d487f1f61783dc91
KhaledAudNada <khaled.nada223@gmail.com>
2021-08-28 21:41:00 +0300

    Build Calculator project uploaded
    Khaled Abu Nada

---
+++ Build Calculator in Python.py
@@ -0,0 +1,8 @@
+num1 = float(input("enter the first no: "))
+num2 = float(input("enter the second no:"))
+
+adding = num1 + num2
+multiplication = num1 * num2
+
+print('Addition', adding)
+print('Multiplication', multiplication)
