aca3d1501707a95419970307d1f32d24178ab094
KhaledAudNada <khaled.nada223@gmail.com>
2021-08-28 23:25:23 +0300

    The code for the Lists Part Two lecture has been uploaded
    Khaled Abu Nada

---
+++ Lists 2.py
@@ -0,0 +1,20 @@
+name = ["Khaled", "Nada", "Abood", "True", 12]
+namber = [1, 158, 65, 89, 99, 7, 36]
+
+# name.append(False)
+name.insert(1, "Aub")
+# print(name)
+
+name.pop()
+name.remove("Aub")
+# name.clear()
+
+print(namber.index(158))
+num_copy = namber.copy()
+namber.append(36)
+namber.count(1)
+print(namber.count(36))
+namber.sort()
+namber.reverse()
+print(namber)
+
