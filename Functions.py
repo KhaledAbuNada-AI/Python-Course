7ea24bc5fdaa27a954101029dee72daf29d526a4
KhaledAudNada <khaled.nada223@gmail.com>
2021-08-30 08:39:59 +0300

    The code for the first part of the Functions lecture has been uploaded
    Khaled Abu Nada

---
+++ Functions.py
@@ -0,0 +1,14 @@
+def sum_number(num1, num2):
+    result = num1 + num2
+    return result
+
+
+print(sum_number(60, 10))
+
+
+def say_hi(name, age):
+    result = "Hello {} your age is {}".format(name, age)
+    return result
+
+
+print(say_hi("KhaledNada", "21"))
