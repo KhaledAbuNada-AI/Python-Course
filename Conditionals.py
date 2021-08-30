f7404da67bfe4c4bb968f65e14de3e2c92af3197
KhaledAudNada <khaled.nada223@gmail.com>
2021-08-30 09:50:17 +0300

    The code for the first part of the Conditionals lecture has been uploaded
    Khaled Abu Nada

---
+++ Conditionals.py
@@ -0,0 +1,26 @@
+is_hungry = True
+wants_to_rat = False
+
+if is_hungry and wants_to_rat:
+    print("Go eat")
+elif is_hungry and not wants_to_rat:
+    print("eat do you can survive")
+else:
+    print("Do not rat")
+
+is_cloudy = False
+
+if is_cloudy:
+    print("I take my umbrella")
+else:
+    print("I take my sunglasses")
+
+is_beef = False
+is_pasta = False
+
+if is_beef:
+    print("I order a Burger")
+elif is_pasta:
+    print("I order Spaghetti")
+else:
+    print("I order a Salad")
