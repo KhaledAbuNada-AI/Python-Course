6d52f63612ac47f9282b459f31059b4905da8ba0
KhaledAudNada <khaled.nada223@gmail.com>
2021-08-31 07:10:25 +0300

    A project to build an advanced calculator intelligently using functions has been uploaded
    Khaled Abu Nada
    
    Khaled Abu Nada

--- Build Calculator 2.py
+++ Build Calculator 2.py
@@ -17,10 +17,29 @@ def division(num1, num2):
     result = num1 / num2
     return result
 
+
 def modulus(num1, num2):
     result = num1 % num2
     return result
 
+
 def exponent(num1, num2):
     result = num1 ** num2
     return result
+
+num1 = float(input("Enter Your Num1: "))
+operator = input("Enter Your Operators: ")
+num2 = float(input("Enter Your Num2: "))
+
+if operator == "+":
+    print(addition(num1, num2))
+elif operator == "-":
+    print(subtraction(num1, num2))
+elif operator == "*":
+    print(division(num1, num2))
+elif operator == "/":
+    print(modulus(num1, num2))
+elif operator == "**":
+    print(exponent(num1, num2))
+else:
+    print("")
